44c1fbc67dc674c82f1d2739507f19ec98bf9843
Matthew McLaren <mmclaren@purdue.edu>
2023-03-27 17:31:58 -0400

    Update test.c
    prints current board

--- test.c
+++ test.c
@@ -6,37 +6,13 @@
 #include "monopoly.h"
 
 int main() {
+    /* Build the placelist */
 
-    /* Open the file */
+    readboardfile(boardfilepath);
 
-    FILE * board_file = NULL;
-    if ((board_file = fopen(boardfilepath, "r")) == NULL) {
-        perror("File can't be read\n");
-        return EXIT_FAILURE;
-    }
-    fscanf(board_file, "%s\n", NULL); // Get rid of the csv header
+    printboardlist(boardlist);
 
-    /* Begin reading the file */
-
-    // char rname[MAX_NAME_LEN] = {'\0'};
-    // int num_record[MAX_NUM_REC_LEN];
-    char buffer[1024] = {'\0'};
-    int checkEOF = 0;
-    while ((checkEOF = fscanf(board_file, "%s\n", buffer) != EOF)) {
-        // TODO : Translate new values into new board places
-
-        // printf("%s, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n",
-        //        rname, num_record[0], num_record[1], num_record[2],
-        //        num_record[3], num_record[4], num_record[5], num_record[6],
-        //        num_record[7], num_record[8], num_record[9], num_record[10]);
-
-    }
-
-    /* Close file */
-
-    fclose(board_file);
-
-    printf("Exited with %d\n", idk());
+    /* Exit blissfully */
 
     return EXIT_SUCCESS;
 }
