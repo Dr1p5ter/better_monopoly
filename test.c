210e86ff00d3fbffb821d7d0a1b5c59056b477fb
Matthew McLaren <mmclaren@purdue.edu>
2023-03-26 20:04:30 -0400

    test board implementation
    checking how fopen works again.

--- test.c
+++ test.c
@@ -7,21 +7,36 @@
 
 int main() {
 
-    place_t * test_place = (place_t *) malloc(sizeof(place_t));
-    test_place->name = strdup("Test Property\0");
-    test_place->rent_list = (unsigned short *) malloc(sizeof(unsigned short) * 5);
-    test_place->rent_list[0] = 0;
-    test_place->rent_list[1] = 1;
-    test_place->rent_list[2] = 2;
-    test_place->rent_list[3] = 3;
-    test_place->rent_list[4] = 4;
-    test_place->color = BLUE;
-    test_place->type = PROPERTY;
-    test_place->price = 500;
-    test_place->m_price = 300;
-    test_place->h_price = 50;
+    /* Open the file */
 
-    printplace(test_place);
+    FILE * board_file = NULL;
+    if ((board_file = fopen(boardfilepath, "r")) == NULL) {
+        perror("File can't be read\n");
+        return EXIT_FAILURE;
+    }
+    fscanf(board_file, "%s\n", NULL); // Get rid of the csv header
 
-    return 0;
+    /* Begin reading the file */
+
+    // char rname[MAX_NAME_LEN] = {'\0'};
+    // int num_record[MAX_NUM_REC_LEN];
+    char buffer[1024] = {'\0'};
+    int checkEOF = 0;
+    while ((checkEOF = fscanf(board_file, "%s\n", buffer) != EOF)) {
+        // TODO : Translate new values into new board places
+
+        // printf("%s, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n",
+        //        rname, num_record[0], num_record[1], num_record[2],
+        //        num_record[3], num_record[4], num_record[5], num_record[6],
+        //        num_record[7], num_record[8], num_record[9], num_record[10]);
+
+    }
+
+    /* Close file */
+
+    fclose(board_file);
+
+    printf("Exited with %d\n", idk());
+
+    return EXIT_SUCCESS;
 }
