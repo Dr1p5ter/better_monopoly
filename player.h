ceed1b7023a8de68547b4e9c91a19e5c713687f2
Matthew McLaren <mmclaren@purdue.edu>
2023-04-03 17:03:15 -0400

    Create player.h

---
+++ player.h
@@ -0,0 +1,15 @@
+/* -- INCLUDES -- */
+
+/* -- DEFINES -- */
+
+/* -- ENUMS -- */
+
+/* -- STRUCTS -- */
+
+typedef struct player {
+    char * name;
+} player_t;
+
+/* -- GLOBALS -- */
+
+/* -- PROTOTYPES -- */
